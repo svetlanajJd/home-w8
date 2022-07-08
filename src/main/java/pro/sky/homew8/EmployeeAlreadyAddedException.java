4e239ef7056aee8a3e4a9a4db857cee82e1144fe
sveta <svetlana.fedoseeva.88@mail.ru>
2022-07-08 20:48:48 +0300

    выполнено дз

---
+++ src/main/java/pro/sky/homew8/EmployeeAlreadyAddedException.java
@@ -0,0 +1,8 @@
+package pro.sky.homew8;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+public class EmployeeAlreadyAddedException extends RuntimeException{
+}
