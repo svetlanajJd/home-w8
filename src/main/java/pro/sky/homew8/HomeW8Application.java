b76b539965afb248c23ad207360610ec52e463e0
sveta <svetlana.fedoseeva.88@mail.ru>
2022-07-08 20:33:34 +0300

    первый коммит

---
+++ src/main/java/pro/sky/homew8/HomeW8Application.java
@@ -0,0 +1,13 @@
+package pro.sky.homew8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeW8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeW8Application.class, args);
+	}
+
+}
